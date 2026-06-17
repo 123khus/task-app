899a950458f329c28455b068f03d60d3970920ec
EC2 Default User <ec2-user@ip-172-31-16-123.ap-southeast-2.compute.internal>
2026-06-17 14:14:44 +0000

    Added PostgreSQL and Docker Compose setup

--- app.py
+++ app.py
@@ -1,21 +1,46 @@
 from flask import Flask, request
-import json
+import psycopg2
 import os
+import time
 
 app = Flask(__name__)
 
-DATA_FILE = "/data/tasks.json"
+def get_connection():
+    for i in range(10):
+        try:
+            conn = psycopg2.connect(
+                host=os.getenv("DB_HOST"),
+                database=os.getenv("DB_NAME"),
+                user=os.getenv("DB_USER"),
+                password=os.getenv("DB_PASSWORD")
+            )
+            print("Connected to PostgreSQL")
+            return conn
 
-def load_tasks():
-    if not os.path.exists(DATA_FILE):
-        return []
+        except psycopg2.OperationalError:
+            print("Waiting for PostgreSQL...")
+            time.sleep(5)
 
-    with open(DATA_FILE) as f:
-        return json.load(f)
+    raise Exception("Could not connect to PostgreSQL")
 
-def save_tasks(tasks):
-    with open(DATA_FILE, "w") as f:
-        json.dump(tasks, f)
+def init_db():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS tasks (
+            id SERIAL PRIMARY KEY,
+            title TEXT NOT NULL,
+            completed BOOLEAN DEFAULT FALSE
+        )
+    """)
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+init_db()
 
 @app.get("/health")
 def health():
@@ -23,22 +48,57 @@ def health():
 
 @app.get("/tasks")
 def get_tasks():
-    return load_tasks()
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT id, title, completed FROM tasks ORDER BY id"
+    )
+
+    rows = cur.fetchall()
+
+    tasks = []
+
+    for row in rows:
+        tasks.append({
+            "id": row[0],
+            "title": row[1],
+            "completed": row[2]
+        })
+
+    cur.close()
+    conn.close()
+
+    return tasks
 
 @app.post("/tasks")
 def create_task():
-    tasks = load_tasks()
+    title = request.json["title"]
 
-    task = {
-        "id": len(tasks) + 1,
-        "title": request.json["title"],
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        """
+        INSERT INTO tasks(title)
+        VALUES(%s)
+        RETURNING id
+        """,
+        (title,)
+    )
+
+    task_id = cur.fetchone()[0]
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    return {
+        "id": task_id,
+        "title": title,
         "completed": False
-    }
-
-    tasks.append(task)
-    save_tasks(tasks)
-
-    return task, 201
+    }, 201
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
